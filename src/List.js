166fb2dfcd71312505e0c1bc84709c8a22653bcb
Alexander Lyon <alyon011@gmail.com>
2018-11-19 19:39:03 -0500

    Separate Task component

--- src/List.js
+++ src/List.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Task } from './Task';
 
 export class List extends React.Component {
   constructor(props) {
@@ -19,12 +20,7 @@ export class List extends React.Component {
     this.formBlur = this.formBlur.bind(this);
     this.getLastUpdate = this.getLastUpdate.bind(this);
     this.updateList = this.updateList.bind(this);
-    this.crossOff = this.crossOff.bind(this);
     this.restoreTask = this.restoreTask.bind(this);
-    this.handleClick = this.handleClick.bind(this);
-    this.taskKeydown = this.taskKeydown.bind(this);
-    this.taskKeyup = this.taskKeyup.bind(this);
-    this.optionsClick = this.optionsClick.bind(this);
     this.deleteFromDatabase = this.deleteFromDatabase.bind(this);
   }
 
@@ -63,133 +59,11 @@ export class List extends React.Component {
   }
 
 
-  crossOff(el) {
-    /* Removes completed task from list */
-    el.classList.add('removed');
-
-    window.setTimeout(() => {
-      let arr = [...this.state.removedList];
-      arr.push(el.querySelector('.task-content').innerText);
-      this.setState({ removedList: arr });
-
-      this.deleteFromDatabase(Number(el.getAttribute('data-note-id')));
-      el.classList.remove('removed');
-    }, 1800);
-  }
-
-
   restoreTask() {
     const recentlyDeleted = [...this.state.removedList];
     const restoredText = recentlyDeleted.pop();
     this.addNewTask(false, restoredText);
     this.setState({ removedList: recentlyDeleted });
-  }
-
-
-  optionsClick(options, taskEl, element, open) {
-    // If optionsBtn
-    if (options.querySelector('.optionsBtn').contains(element)) {
-      if (open) {
-        // Close options menu
-        taskEl.classList.remove('options-open');
-      }
-      else {
-        // Open options menu
-        taskEl.classList.add('options-open');
-      }
-    }
-    else if (options.querySelector('.editBtn').contains(element)) {
-      // Edit clicked
-      if (taskEl.classList.contains('edit-mode')) {
-        // Disable editing
-        taskEl.classList.remove('edit-mode');
-        taskEl.querySelector('.task-content').blur();
-        taskEl.querySelector('.task-content').setAttribute('contenteditable', false);
-        taskEl.querySelector('.editBtn i').classList.replace('fa-save', 'fa-pencil-alt');
-      }
-      else {
-        // Enable editing
-        taskEl.classList.add('edit-mode');
-        taskEl.querySelector('.task-content').setAttribute('contenteditable', true);
-        taskEl.querySelector('.editBtn i').classList.replace('fa-pencil-alt', 'fa-save');
-        taskEl.querySelector('.task-content').focus();
-
-        // Move caret:
-        let textNode = taskEl.querySelector('.task-content').firstChild;
-        let caret = textNode.length;
-        let range = document.createRange();
-        range.setStart(textNode, caret);
-        range.setEnd(textNode, caret);
-        let sel = window.getSelection();
-        sel.removeAllRanges();
-        sel.addRange(range);
-      }
-    }
-    else if (options.querySelector('.deleteBtn').contains(element)) {
-      // Delete clicked
-      let arr = [...this.state.removedList];
-      arr.push(taskEl.querySelector('.task-content').innerText);
-      this.setState({ removedList: arr });
-
-      this.deleteFromDatabase(Number(taskEl.getAttribute('data-note-id')));
-    }
-  }
-
-
-  handleClick(e) {
-    e.stopPropagation();
-    const element = e.currentTarget;
-
-    if (element.classList.contains('options')) {
-      const taskEl = element.parentElement;
-      const open = taskEl.classList.contains('options-open') ? true : false;
-      this.optionsClick(element, taskEl, e.target, open);
-    }
-    else if (element.classList.contains('edit-mode')) {
-      // Editable, ignore click
-    }
-    else if (!element.classList.contains('edit-mode')) {
-      this.crossOff(element);
-    }
-  }
-
-
-  taskKeydown(e) {
-    if (e.keyCode === 13) {
-      e.preventDefault();
-      const el = e.currentTarget;
-
-      // Disable editing
-      el.classList.remove('edit-mode');
-      el.querySelector('.task-content').blur();
-      el.querySelector('.task-content').setAttribute('contenteditable', false);
-      el.querySelector('.editBtn i').classList.replace('fa-save', 'fa-pencil-alt');
-      el.querySelector('.optionsBtn').click();
-    }
-  }
-
-  taskKeyup(e) {
-    // Edit database entry
-    const el = e.currentTarget;
-    let newText = el.innerText;
-    let key = el.getAttribute('data-note-id');
-    let transaction = this.props.database.transaction(['notes'], 'readwrite');
-    let objectStore = transaction.objectStore('notes');
-
-    objectStore.openCursor().onsuccess = (event) => {
-      const cursor = event.target.result;
-      if (cursor) {
-        if (cursor.key == key) {
-          const updateData = cursor.value;
-          updateData.body = newText;
-          const request = cursor.update(updateData);
-          request.onsuccess = function() {
-            // Database updated successfully
-          };
-        }
-        cursor.continue();
-      }
-    };
   }
 
 
@@ -207,7 +81,12 @@ export class List extends React.Component {
   }
 
 
-  deleteFromDatabase(noteID) {
+  deleteFromDatabase(el) {
+    const arr = [...this.state.removedList];
+    arr.push(el.querySelector('.task-content').innerText);
+    this.setState({ removedList: arr });
+
+    const noteID = Number(el.getAttribute('data-note-id'));
     let transaction = this.props.database.transaction(['notes'], 'readwrite');
     let objectStore = transaction.objectStore('notes');
     let request = objectStore.delete(noteID);
@@ -221,18 +100,10 @@ export class List extends React.Component {
 
   printTasks() {
     const fetchedTasks = this.state.taskList.map( (entry) => (
-      <li
-        onClick={this.handleClick}
-        onKeyDown={this.taskKeydown}
-        onKeyUp={this.taskKeyup}
-        data-note-id={entry.id}>
-        <span className='task-content'>{entry.body}</span>
-        <span className='options' onClick={this.handleClick}>
-          <span className='editBtn'><i className='fas fa-pencil-alt'></i></span>
-          <span className='deleteBtn'><i className='fas fa-trash'></i></span>
-          <span className='optionsBtn'><i className='fas fa-ellipsis-v'></i></span>
-        </span>
-     </li>
+      <Task entryID={entry.id} entryBody={entry.body}
+        database={this.props.database}
+        removeTask={this.deleteFromDatabase}
+      />
     ));
 
     return fetchedTasks;
---
+++ src/Task.js
@@ -0,0 +1,149 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export class Task extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+
+    this.handleClick = this.handleClick.bind(this);
+    this.taskKeydown = this.taskKeydown.bind(this);
+    this.taskKeyup = this.taskKeyup.bind(this);
+    this.optionsClick = this.optionsClick.bind(this);
+    this.crossOff = this.crossOff.bind(this);
+  }
+
+
+  handleClick(e) {
+    e.stopPropagation();
+    const element = e.currentTarget;
+
+    if (element.classList.contains('options')) {
+      const taskEl = element.parentElement;
+      const open = taskEl.classList.contains('options-open') ? true : false;
+      this.optionsClick(element, taskEl, e.target, open);
+    }
+    else if (element.classList.contains('edit-mode')) {
+      // Editable, ignore click
+    }
+    else if (!element.classList.contains('edit-mode')) {
+      this.crossOff(element);
+    }
+  }
+
+
+  taskKeydown(e) {
+    if (e.keyCode === 13) {
+      e.preventDefault();
+      const el = e.currentTarget;
+
+      // Disable editing
+      el.classList.remove('edit-mode');
+      el.querySelector('.task-content').blur();
+      el.querySelector('.task-content').setAttribute('contenteditable', false);
+      el.querySelector('.editBtn i').classList.replace('fa-save', 'fa-pencil-alt');
+      el.querySelector('.optionsBtn').click();
+    }
+  }
+
+
+  taskKeyup(e) {
+    // Edit database entry
+    const el = e.currentTarget;
+    let newText = el.innerText;
+    let key = el.getAttribute('data-note-id');
+    let transaction = this.props.database.transaction(['notes'], 'readwrite');
+    let objectStore = transaction.objectStore('notes');
+
+    objectStore.openCursor().onsuccess = (event) => {
+      const cursor = event.target.result;
+      if (cursor) {
+        if (cursor.key == key) {
+          const updateData = cursor.value;
+          updateData.body = newText;
+          const request = cursor.update(updateData);
+          request.onsuccess = function() {
+            // Database updated successfully
+          };
+        }
+        cursor.continue();
+      }
+    };
+  }
+
+
+  optionsClick(options, taskEl, element, open) {
+    // If optionsBtn
+    if (options.querySelector('.optionsBtn').contains(element)) {
+      if (open) {
+        // Close options menu
+        taskEl.classList.remove('options-open');
+      }
+      else {
+        // Open options menu
+        taskEl.classList.add('options-open');
+      }
+    }
+    else if (options.querySelector('.editBtn').contains(element)) {
+      // Edit clicked
+      if (taskEl.classList.contains('edit-mode')) {
+        // Disable editing
+        taskEl.classList.remove('edit-mode');
+        taskEl.querySelector('.task-content').blur();
+        taskEl.querySelector('.task-content').setAttribute('contenteditable', false);
+        taskEl.querySelector('.editBtn i').classList.replace('fa-save', 'fa-pencil-alt');
+      }
+      else {
+        // Enable editing
+        taskEl.classList.add('edit-mode');
+        taskEl.querySelector('.task-content').setAttribute('contenteditable', true);
+        taskEl.querySelector('.editBtn i').classList.replace('fa-pencil-alt', 'fa-save');
+        taskEl.querySelector('.task-content').focus();
+
+        // Move caret:
+        let textNode = taskEl.querySelector('.task-content').firstChild;
+        let caret = textNode.length;
+        let range = document.createRange();
+        range.setStart(textNode, caret);
+        range.setEnd(textNode, caret);
+        let sel = window.getSelection();
+        sel.removeAllRanges();
+        sel.addRange(range);
+      }
+    }
+    else if (options.querySelector('.deleteBtn').contains(element)) {
+      // Delete clicked
+      this.props.removeTask(taskEl);
+    }
+  }
+
+
+  crossOff(el) {
+    /* Removes completed task from list */
+    el.classList.add('removed');
+
+    window.setTimeout(() => {
+      this.props.removeTask(el);
+      el.classList.remove('removed');
+    }, 1800);
+  }
+
+
+  render() {
+    return (
+      <li
+        onClick={this.handleClick}
+        onKeyDown={this.taskKeydown}
+        onKeyUp={this.taskKeyup}
+        data-note-id={this.props.entryID}>
+        <span className='task-content'>{this.props.entryBody}</span>
+        <span className='options' onClick={this.handleClick}>
+          <span className='editBtn'><i className='fas fa-pencil-alt'></i></span>
+          <span className='deleteBtn'><i className='fas fa-trash'></i></span>
+          <span className='optionsBtn'><i className='fas fa-ellipsis-v'></i></span>
+        </span>
+     </li>
+    );
+  }
+
+}
